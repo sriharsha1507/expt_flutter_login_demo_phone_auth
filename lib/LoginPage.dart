3869b49046ed4127113243225d239bfcab79c208
sriharsha1507 <sriharsha1507@gmail.com>
2018-09-15 20:35:50 -0500

    *Added mobile number field using form widget with mobile number validation *maxlength count display should be removed from UI

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,12 +7,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
+  String _mobileNumber;
+
+  void validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      print("Form is valid  $_mobileNumber");
+    } else {
+      print("Form is invalid");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: new Text('Flutter Phone Auth Demo')),
       body: new Container(
-        child: Text("Hello World"),
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              new TextFormField(
+                decoration: new InputDecoration(labelText: 'Mobile number'),
+                inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                keyboardType: TextInputType.phone,
+                validator: (number) {
+                  if (number.isEmpty) {
+                    return 'Mobile number cannot be empty';
+                  } else if (number.length < 10) {
+                    return 'Mobile number is invalid';
+                  } else {
+                    return null;
+                  }
+                },
+                maxLength: 10,
+                onSaved: (value) => _mobileNumber = value,
+              ),
+              new RaisedButton(
+                child: new Text(
+                  'Login',
+                  style: new TextStyle(fontSize: 20.0),
+                ),
+                onPressed: validateAndSave,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
